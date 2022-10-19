f581cf1b0c6520aace3981f4cba18b1050b779f4
sofiazulfiqar <92971985+sofiazulfiqar@users.noreply.github.com>
2022-10-19 11:05:28 -0700

    Create file.js

---
+++ file.js
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title></title>
+<!-- Script tag can also be placed here -->
+</head>
+<body>
+
+	
+
+<p>Before the script...</p>
+
+
+<!-- Script tag inside the body -->
+<script>
+	// write the javascript code inside it
+</script>
+
+	
+
+<p>...After the script.</p>
+
+
+
+</body>
+</html>
